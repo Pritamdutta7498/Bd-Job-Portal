381ee3a58afa7b14a4da1369dcd0d46a9e3d38a6
PritamDutta <webdevpritam7498@gmail.com>
2023-09-12 00:34:22 +0600

    jobs section added in home routes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Category from '../Category/Category';
+import Jobs from '../Jobs/Jobs';
+
 
 const Home = () => {
     return (
         <div>
            <Banner/>
            <Category/>
+           <Jobs/>
         </div>
     );
 };
