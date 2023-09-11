f180d09d60a155406a235a1239d3a3d484dcae79
PritamDutta <webdevpritam7498@gmail.com>
2023-09-12 01:02:47 +0600

    added some page for initial setup

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,19 +1,52 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layout/Main/MainLayout";
 import Home from "../pages/Home/Home";
-
+import Services from "../pages/Services/Services";
+import AddJob from "../pages/AddJob/AddJob";
+import MyJobs from "../pages/MyJobs/MyJobs";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
     children: [
-        {
-            path:'/',
-            element:<Home/>
-        },
-        
-    ]
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "home",
+        element: <Home></Home>,
+      },
+      {
+        path: "services",
+        element: <Services />,
+      },
+      {
+        path: "/addJob",
+        element: <AddJob />,
+      },
+      {
+        path: "/MyJobs",
+        element: <MyJobs/>
+      },
+      //   {
+      //     path: "services/:id",
+      //     element: (
+      //       <PrivateRoute>
+      //         <ServiceDetails></ServiceDetails>
+      //       </PrivateRoute>
+      //     ),
+      //   },
+      //   {
+      //     path: "login",
+      //     element: <Login></Login>,
+      //   },
+      //   {
+      //     path: "register",
+      //     element: <Register></Register>,
+      //   },
+    ],
   },
 ]);
 export default router;
