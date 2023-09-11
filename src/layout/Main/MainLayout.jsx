00da21a73bb2d98a2d92d11acc7a01397cbda91a
PritamDutta <webdevpritam7498@gmail.com>
2023-09-11 23:43:52 +0600

    setup manubar and footer component

--- src/layout/Main/MainLayout.jsx
+++ src/layout/Main/MainLayout.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Menubar from "../../pages/Menubar/Menubar";
+import Footer from "../../pages/Footer/Footer";
 
 const MainLayout = () => {
   return (
     <div>
+      <Menubar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
---
+++ src/pages/Menubar/Menubar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Menubar = () => {
+  return (
+    <div className="Menubar-container">
+      <div className="logo">
+        <img
+          className="logo"
+          src="https://cdn-icons-png.flaticon.com/512/4017/4017739.png"
+          alt=""
+        />
+      </div>
+      <div className="menu-container">
+        <Link className="menu-item" to="/">
+          <li>Home</li>
+        </Link>
+        <Link className="menu-item" to="/MyJobs">
+          <li>My Job</li>
+        </Link>
+        <Link className="menu-item" to="/addJob">
+          <li>Post Job</li>
+        </Link>
+
+        <Link className="menu-item" to="/login">
+          <li>Login</li>
+        </Link>
+
+        <Link className="menu-item" to="/register">
+          <li>Registration</li>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Menubar;
